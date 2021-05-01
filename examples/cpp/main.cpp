dcbd9bc815c957ad7a28b1b5100cd7f587f40dfa
hdevarajan <hdevarajan@penguin>
2021-05-01 13:11:55 -0500

    added formatting and some comments for functions.

--- examples/cpp/main.cpp
+++ examples/cpp/main.cpp
@@ -1,21 +1,24 @@
-#include "mpi.h"
 #include <stdio.h>
 #include <unistd.h>
 #include <mpi.h>
 
 int main(){
   MPI_Init(NULL, NULL);
+  /* Get Rank and Comm from MPI*/
   int rank, comm_sz;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
-
+  /* Get PID from process. */
   int pid = getpid();
   printf("%d\n", pid);
   fflush(stdout);
-  if(rank == 0) {
+  if (rank == 0) {
+    printf("press any key to continue executation.\n");
+    fflush(stdout);
     getchar();
   }
   MPI_Barrier(MPI_COMM_WORLD);
+  /* App logic goes here. */
   printf("hello1\n");
   printf("hello2\n");
   printf("hello3\n");
